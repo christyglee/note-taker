b84fc322a89b751b40f7d49e27e36a889fff23bb
unknown <christy.g.lee@gmail.com>
2020-10-15 23:27:27 -0700

    established dependendencies, express, data parsing

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // dependencies
 var express = require("express");
 var path = require("path");
-var fs = require("fs")
-var database = require("./db/db.json")
+var fs = require("fs");
+var db = require("./db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
 // set up express
 var app = express();
@@ -11,77 +12,6 @@ var PORT = process.env.PORT || 3000;
 // handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.stsatic("public"))
-
-// notes arrays
-var notes = [];
+app.use(express.static("public"))
 
 
-// get routes/redirects
-// index.html
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-// notes.html
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-});
-
-// api notes
-app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
-});
-
-
-// api/clear clear tables!
-// app.put("/api/clear", function(req, res) {
-//     tables = [];
-//     waitlist = [];
-//     res.send("clear")
-// })
-
-// api/delete/table
-app.delete("/api/delete/table", function(req, res) {
-    var index = req.body.index;
-    var temp = [];
-    for (var i = 0; i < tables.length; i++) {
-        if (i !== parseInt(index)) {
-          temp.push(tables[i]);
-        }
-    }
-    tables = temp;
-    res.send("table removed")
-})
-
-// apie/delete/waitlist
-app.delete("/api/delete/waitlist", function(req, res) {
-    var index = req.body.index;
-    var temp = [];
-    for (var i = 0; i < tables.length; i++) {
-        if (i !== parseInt(index)) {
-          temp.push(waitlist[i]);
-        }
-    }
-    waitlist = temp;
-    res.send("waitlist removed")
-})
-
-// post api/tables
-app.post("/api/tables", function(req, res) {
-    var newTable = req.body;
-    console.log(newTable);
-    if (tables.length < 5 ) {
-        tables.push(newTable);
-        res.send("Table")
-    } else {
-        waitlist.push(newTable);
-        res.send();
-    }
-});
-
-
-// start server
-app.listen(PORT, function() {
-    console.log("App listening on PORT http://localhost:" + PORT)
-})
